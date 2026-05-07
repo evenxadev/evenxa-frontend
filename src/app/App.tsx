0b6d96b22a30483aff1f3dee8a29a079093097b6
leolopezOsornio <leol83377@gmail.com>
2026-05-07 16:55:12 -0600

    feat(landing):change default route, from home to landing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
 import { Header } from "../widgets/header/ui/header";
-//import { LandingPage } from "../pages/landing";
+import { LandingPage } from "../pages/landing";
 import { AppProviders } from "./providers";
 import { AppRouter } from "./router";
-//import { routes } from "./router/routes";
+import { routes } from "./router/routes";
 import "./styles/index.css";
 
 export function App() {
-    // const showProductApp = window.location.pathname.startsWith(routes.app);
+    const showProductApp = window.location.pathname.startsWith(routes.app);
 
-    // if (!showProductApp) {
-    //     return <LandingPage />;
-    // }
+    if (!showProductApp) {
+        return <LandingPage />;
+    }
 
     return (
         <>
